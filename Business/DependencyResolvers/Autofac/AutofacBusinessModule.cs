47d6064d96c92e0ed70a3a5d87e191573f4e8586
hakan cirit <hakan-cirit-1905@hotmail.com>
2021-09-10 21:52:18 +0300

    Ioc konteynır için Autofac paketi kuruldu

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EfAboutDal>().As<IAboutDal>().SingleInstance();
+            builder.RegisterType<EfBlogDal>().As<IBlogDal>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+            builder.RegisterType<EfCommentDal>().As<ICategoryDal>().SingleInstance();
+            builder.RegisterType<EfContactDal>().As<IContactDal>().SingleInstance();
+            builder.RegisterType<EfWriterDal>().As<IWriterDal>().SingleInstance();
+
+        }
+    }
+}
