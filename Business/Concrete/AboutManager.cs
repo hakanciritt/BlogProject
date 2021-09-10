8394d440d68b6199313a7c0a56ed294e41a38cd7
hakan cirit <hakan-cirit-1905@hotmail.com>
2021-09-10 22:11:27 +0300

    Manager ve Service sınıfları eklendi ve constants içerisinde mesajlar oluşturuldu

---
+++ Business/Concrete/AboutManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        public IResult Add(About about)
+        {
+            _aboutDal.Add(about);
+            return new SuccessResult(Messages.AboutAdded);
+        }
+
+        public IResult Delete(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(About about)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
