6b7aec133266a78c50cada30e85892e00c45bb20
Hakan Cirit <hakanciritt@gmail.com>
2021-12-24 23:46:06 +0300

    hava durumu çekme ve gösterme tamamlandı

--- Blog.Dashboard/Components/StatisticViewComponent.cs
+++ Blog.Dashboard/Components/StatisticViewComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Dashboard.Components
@@ -10,6 +11,14 @@ namespace Blog.Dashboard.Components
     {
         public IViewComponentResult Invoke()
         {
+            string apiKey = "75cd4d41e3f23a1e993df9aaef65ee59";
+            string cityName = "istanbul";
+
+            string connection =
+                $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&mode=xml&appid={apiKey}";
+            XDocument document = XDocument.Load(connection);
+            ViewBag.weather = document.Descendants("temperature").ElementAt(0).Attribute("value")?.Value;
+
             return View();
         }
     }
